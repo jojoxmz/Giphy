d267dffe0d9954e783ebcbe53a3884aada67764a
JoJo Zheng^C <jojozheng1216@hotmail.com>
2018-01-07 13:35:45 -0700

    added final details

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,11 +1,4 @@
-// $(document).ready(function(){
-// $(‘.carousel.carousel-slider’).carousel({fullWidth: true});
-
-
-// });
-
-var topics=["tigers","sugar gliders", "otters", "chinchillas", "dragons"];
-
+var topics=["tigers","sugar gliders", "otters", "chinchillas", "leopards", "giraffs", "pandas"];
 
 function createButtons(){
 	$('.buttons').empty();
@@ -17,15 +10,16 @@ function createButtons(){
 createButtons();
 
 function createNewButton(){
-	var newButtonText= $('#searchInput').val();
+	var newButtonText= $('#searchInput').val().toLowerCase();
 	console.log(newButtonText);
 	topics.push(newButtonText);
 	createButtons()
 	};
 
-$(".buttons").on("click", "button", function(e) {
-	e.preventDefault();
+$(".buttons").on("click", "button", function(event) {
+	event.preventDefault();
 	$('.coolGifs').empty();
+
 	var buttonText = $(this).attr("data-name");
 	var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + buttonText + "&limit=10&api_key=dc6zaTOxFJmzC";
 
@@ -34,13 +28,8 @@ $(".buttons").on("click", "button", function(e) {
 		method: 'GET'
 	}).done(function(response) {
 		console.log(response);
-		for (var i = 0; i < response.data.length; i++) {
 
-			// HEY GUYS! See below for the stuff of interest!
-			// The code below does the same thing that the messy code 
-			// block we had before does... but it's a lot cleaner
-			// and easier to make sense of. Message me if you have
-			// any questions on the what/why/how of it!
+		for (var i = 0; i < response.data.length; i++) {
 
 			// first, each time we loop through, we're creating a new img tag
 			var newDiv= $("<div />").addClass("divContainer");
@@ -54,43 +43,34 @@ $(".buttons").on("click", "button", function(e) {
 				.attr("data-still", response.data[i].images.fixed_height_small_still.url)
 				.attr("data-anim", response.data[i].images.fixed_height_small.url)
 				.attr("src", response.data[i].images.fixed_height_small_still.url)
-				// .attr("align", "left");
-				// .attr("ratings", response.data[i].rating)
 			// then, the last thing we do is we append the newImg to the coolGifs container
 			newDiv.append(newImg).append(p);
 
 			$(".coolGifs").append(newDiv);
-
-
 		}
 	});
 
 });
 
-$(".coolGifs").on("click", ".giphy", function(e) {
-	// clear the coolGifs container first!!!
-	
-	e.preventDefault();
+$(".coolGifs").on("click", ".giphy", function(event) {
+	event.preventDefault();
 
 	var gifState = $(this).attr("data-state");
 	var animUrl = $(this).attr("data-anim");
 	var stillUrl = $(this).attr("data-still");
 
 	if (gifState == "still") {
-		$(this).attr("src", animUrl).attr('data-state', 'animate');
 		 // this sets the src attribute to the animated url
 		// set 'data-state' to be equal to 'anim'
-
+		$(this).attr("src", animUrl).attr('data-state', 'animate');
 	} else {
-		$(this).attr("src", stillUrl).attr('data-state', 'still');
 		// set src to be equal to value to data-still
 		// set 'data-state' to be equal to still
+		$(this).attr("src", stillUrl).attr('data-state', 'still');
 	}
-
 });
 
-// Add a click event for the submit button in the 'add animal' section
-// This click event should call the 'createNewButton' function
+// Add a click event for the submit button 
 $("#submitButton").on("click", function(e){
 	e.preventDefault();
 	console.log("hey");
